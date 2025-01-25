0264dc43e6fe45d19a43a5e6e77d89162931b077
ursulapineda <ursulapineda702@yahoo.com>
2025-01-25 05:06:59 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('enterGiveaway').addEventListener('click', () => {
+    alert('You have entered the giveaway!');
+});
+
+document.getElementById('contactForm').addEventListener('submit', (e) => {
+    e.preventDefault();
+    alert('Your message has been sent!');
+});
